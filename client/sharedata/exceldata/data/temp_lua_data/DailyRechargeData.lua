b4b37b985bcf26a0569d94f5320a0bc07379c164
zhangjian <jian.zhang@gaea.com>
2021-10-30 17:35:59 +0800

    sharedata-exceldata

---
+++ client/sharedata/exceldata/data/temp_lua_data/DailyRechargeData.lua
@@ -0,0 +1,28 @@
+return {
+    [ [==[__element_names_scheme]==] ] = {
+        [1] = [==[id]==],
+        [2] = [==[activity_time_list]==],
+        [3] = [==[recharge_list]==],
+        [4] = [==[reward_list]==],
+        [5] = [==[recharge_days]==],
+        [6] = [==[luxury_reward]==],
+    },
+    [ [==[__table_field_list]==] ] = {
+    },
+    [ 1 ] = {
+        recharge_list = {1,2,3,4,5,6,7,},
+        recharge_days = 5,
+        luxury_reward = 4015,
+        activity_time_list = {[==[2019-05-10]==],[==[2019-05-17]==],},
+        reward_list = {4001,4002,4003,4004,4005,4006,4007,},
+        id = 1,
+    },
+    [ 2 ] = {
+        recharge_list = {1,2,3,4,5,6,7,},
+        recharge_days = 5,
+        luxury_reward = 4016,
+        activity_time_list = {[==[2019-06-10]==],[==[2019-06-17]==],},
+        reward_list = {4008,4009,4010,4011,4012,4013,4014,},
+        id = 2,
+    },
+}
