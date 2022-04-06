4610ab716d0ef26df2f9247a77d5c5bda4d042ef
zhangjian <jian.zhang@gaea.com>
2022-04-06 22:54:41 +0800

    情人礼包和英雄礼包物品信息更改

--- client/client/LuaScript/UI/HeroGiftUI.lua
+++ client/client/LuaScript/UI/HeroGiftUI.lua
@@ -8,7 +8,6 @@ function HeroGiftUI:DoInit()
     HeroGiftUI.super.DoInit(self)
     self.dy_vip_data = ComMgrs.dy_data_mgr.vip_data
     self.prefab_path = "UI/Common/HeroGiftUI"
-    self.index = 1
 end
 
 function HeroGiftUI:OnGoLoadedOk(res_go)
@@ -57,11 +56,7 @@ function HeroGiftUI:InitRes()
 
     --添加美女
     self.unit_rect = content:FindChild("UnitRect")
-    self.check_item_list = content:FindChild("ItemCheckList/ViewPort/CheckItemList")
-    self.reward_item = content:FindChild("ItemCheckList/ViewPort/CheckItemList/RewardItem")
     self.cur_frame_obj_list = {}
-    --self:UpdateData()
-    --self:InitLoverItem()
 
     self.left_btn = content:FindChild("ButtonLeft")
     self:AddClick(self.left_btn, function ()
@@ -89,10 +84,11 @@ function HeroGiftUI:UpdateHeroInfo(index)
             self.unit = self:AddFullUnit(hero_unit_id, self.unit_rect)
             --获得道具
             local item_list = hero_info.item_list
-            self:InitLoverItemNew(item_list)
+            print("英雄礼包道具信息----",item_list)
+            self.cur_frame_obj_list = UIFuncs.SetItemList(self, item_list, self.check_item_list)
             --礼包价格
             local price = hero_info.price
-            self.buyText.text = "US$" .. price
+            self.buyText.text = price .. "JG"
             --限购次数（当前/总数）
             --local cur_purchase_count = lover_info.cur_purchase_count
             local purchase_count = hero_info.purchase_count
@@ -108,20 +104,8 @@ function HeroGiftUI:UpdateHeroInfo(index)
     end
 end
 
-function HeroGiftUI:InitLoverItemNew(item_list)
-    print("HeroGiftUI:InitLoverItemNew(item_list)-------------",item_list)
-    for i in ipairs(item_list) do
-        local item = self:GetUIObject(self.reward_item, self.check_item_list)
-        table.insert(self.cur_frame_obj_list, item)
-        print("道具信息---",item_list[i].item_id,item_list[i].count)
-        UIFuncs.AssignItem(item, item_list[i].item_id, item_list[i].count)
-    end
-end
-
 function HeroGiftUI:InitUI()
     self:InitTaskInfo()
-    self:UpdateData()
-    --self:InitLoverItem()
     if self.activity_list_length ~= nil then
         self:UpdateHeroInfo(1)
     end
@@ -165,88 +149,16 @@ function HeroGiftUI:UpdateMiddle(index)
     end
 end
 
-function HeroGiftUI:UpdateData()
-    self.check_data = ComMgrs.dy_data_mgr.check_data
-    print("情人111---",self.check_data)
-    self.dy_activity_data = ComMgrs.dy_data_mgr.activity_data
-    print("情人222---",self.dy_activity_data)
-    self.month_check_info = self.check_data.month_check_info
-    print("情人333---",self.month_check_info)
-    self.month = Time:GetServerDate().month
-    self.day = Time:GetServerDate().day
-    self.check_month_data = SpecMgrs.data_mgr:GetCheckInMonthlyData(self.month)
-    print("情人444---",self.check_month_data)
-end
-
-function HeroGiftUI:InitLoverItem()
-    print("道具1111--------",self.month_check_info.check_in_date_reward)
-    for i, state in ipairs(self.month_check_info.check_in_date_reward) do
-        if not self.check_month_data.reward_id[i] then return end
-        local item = self:GetUIObject(self.reward_item, self.check_item_list)
-        table.insert(self.cur_frame_obj_list, item)
-        print("道具信息---",self.check_month_data.reward_id[i],self.check_month_data.reward_count[i])
-        UIFuncs.AssignItem(item, self.check_month_data.reward_id[i], self.check_month_data.reward_count[i])
---
---        local can_check_image = item:FindChild("CanCheckImage")
---        local vip = item:FindChild("Vip")
---        local have_check_image = item:FindChild("HaveCheckImage")
---        local make_up = item:FindChild("MakeUp")
---
---        if table.contains(self.check_month_data.vip_day, i) then
---            vip:SetActive(true)
---            local index = table.index(self.check_month_data.vip_day, i)
---            vip:FindChild("VipText"):GetComponent("Text").text = string.format(UIConst.Text.VIP_DOUBLE_FORMAT, self.check_month_data.vip_level_request[index])
---        else
---            vip:SetActive(false)
---        end
---
---        item:FindChild("DayText"):GetComponent("Text").text = string.format(UIConst.Text.CHECK_DAY_FORMAT, i)
---
---        can_check_image:SetActive(false)
---        have_check_image:SetActive(false)
---        make_up:SetActive(false)
---
---        if state == CSConst.RewardState.unpick then
---            self:AddClick(item, function()
---                SpecMgrs.ui_mgr:ShowItemPreviewUI(self.check_month_data.reward_id[i])
---            end)
---        elseif state == CSConst.RewardState.pick then
---            if self.day == i then
---                can_check_image:SetActive(true)
---                local gold_effect_parent = item
---                local glod_circle_effect = UIFuncs.AddGlodCircleEffect(self, item)
---                self.cur_frame_effect_dict[glod_circle_effect] = gold_effect_parent
---            else
---                make_up:SetActive(true)
---            end
---            self:AddClick(item, function()
---                self:ClickMonthCheck(item, i)
---            end)
---        elseif state == CSConst.RewardState.picked then
---            have_check_image:SetActive(true)
---            self:AddClick(item, function()
---                SpecMgrs.ui_mgr:ShowItemPreviewUI(self.check_month_data.reward_id[i])
---            end)
---        end
-    end
---    local rect = self.check_item_list:GetComponent("RectTransform")
---    rect.anchoredPosition = Vector2.New(rect.anchoredPosition.x, 0)
-end
-
 function HeroGiftUI:InitTaskInfo()
     self.left_btn:SetActive(false)
     self.right_btn:SetActive(false)
 
     self.index = 1
     self:UpdateMiddle(1)
-
 end
 
 function HeroGiftUI:ClearInfo()
-    for _, item in pairs(self.cur_frame_obj_list) do
-        self:DelUIObject(item)
-    end
-    self.cur_frame_obj_list = {}
+    self:DelObjDict(self.cur_frame_obj_list)
 end
 
 function HeroGiftUI:ClearRes()
--- client/client/LuaScript/UI/LoverGiftUI.lua
+++ client/client/LuaScript/UI/LoverGiftUI.lua
@@ -8,8 +8,6 @@ function LoverGiftUI:DoInit()
     LoverGiftUI.super.DoInit(self)
     self.dy_vip_data = ComMgrs.dy_data_mgr.vip_data
     self.prefab_path = "UI/Common/LoverGiftUI"
-
-    self.lover_unit_dict = {}
 end
 
 function LoverGiftUI:OnGoLoadedOk(res_go)
@@ -58,8 +56,6 @@ function LoverGiftUI:InitRes()
 
     --添加美女
     self.unit_rect = content:FindChild("UnitRect")
-    self.check_item_list = content:FindChild("ItemCheckList/ViewPort/CheckItemList")
-    self.reward_item = content:FindChild("ItemCheckList/ViewPort/CheckItemList/RewardItem")
     self.cur_frame_obj_list = {}
 
     self.left_btn = content:FindChild("ButtonLeft")
@@ -71,8 +67,6 @@ function LoverGiftUI:InitRes()
     self:AddClick(self.right_btn, function ()
         self:RightButton()
     end)
-
-
 
     self.check_item_list = content:FindChild("ItemCheckList/ViewPort/CheckItemList")
     self.reward_item = content:FindChild("ItemCheckList/ViewPort/CheckItemList/RewardItem")
@@ -92,10 +86,11 @@ function LoverGiftUI:UpdateLoverInfo(index)
             self.unit = self:AddFullUnit(lover_unit_id, self.unit_rect)
             --获得道具
             local item_list = lover_info.item_list
-            self:InitLoverItemNew(item_list)
+            print("情人礼包道具信息----",item_list)
+            self.cur_frame_obj_list = UIFuncs.SetItemList(self, item_list, self.check_item_list)
             --礼包价格
             local price = lover_info.price
-            self.buyText.text = "US$" .. price
+            self.buyText.text = price .. "JG"
             --限购次数（当前/总数）
             --local cur_purchase_count = lover_info.cur_purchase_count
             local purchase_count = lover_info.purchase_count
@@ -112,20 +107,8 @@ function LoverGiftUI:UpdateLoverInfo(index)
     end
 end
 
-function LoverGiftUI:InitLoverItemNew(item_list)
-    print("LoverGiftUI:InitLoverItemNew(item_list)-------------",item_list)
-    for i in ipairs(item_list) do
-        local item = self:GetUIObject(self.reward_item, self.check_item_list)
-        table.insert(self.cur_frame_obj_list, item)
-        print("道具信息---",item_list[i].item_id,item_list[i].count)
-        UIFuncs.AssignItem(item, item_list[i].item_id, item_list[i].count)
-    end
-end
-
 function LoverGiftUI:InitUI()
     self:InitTaskInfo()
-    self:UpdateData()
-    --self:InitLoverItem()
     if self.activity_list_length ~= nil then
         self:UpdateLoverInfo(1)
     end
@@ -169,74 +152,6 @@ function LoverGiftUI:UpdateMiddle(index)
     end
 end
 
-function LoverGiftUI:UpdateData()
-    self.check_data = ComMgrs.dy_data_mgr.check_data
-    print("情人111---",self.check_data)
-    self.dy_activity_data = ComMgrs.dy_data_mgr.activity_data
-    print("情人222---",self.dy_activity_data)
-    self.month_check_info = self.check_data.month_check_info
-    print("情人333---",self.month_check_info)
-    self.month = Time:GetServerDate().month
-    self.day = Time:GetServerDate().day
-    self.check_month_data = SpecMgrs.data_mgr:GetCheckInMonthlyData(self.month)
-    print("情人444---",self.check_month_data)
-end
-
-function LoverGiftUI:InitLoverItem()
-    print("道具1111--------",self.month_check_info.check_in_date_reward)
-    for i, state in ipairs(self.month_check_info.check_in_date_reward) do
-        if not self.check_month_data.reward_id[i] then return end
-        local item = self:GetUIObject(self.reward_item, self.check_item_list)
-        table.insert(self.cur_frame_obj_list, item)
-        print("道具信息---",self.check_month_data.reward_id[i],self.check_month_data.reward_count[i])
-        UIFuncs.AssignItem(item, self.check_month_data.reward_id[i], self.check_month_data.reward_count[i])
---
---        local can_check_image = item:FindChild("CanCheckImage")
---        local vip = item:FindChild("Vip")
---        local have_check_image = item:FindChild("HaveCheckImage")
---        local make_up = item:FindChild("MakeUp")
---
---        if table.contains(self.check_month_data.vip_day, i) then
---            vip:SetActive(true)
---            local index = table.index(self.check_month_data.vip_day, i)
---            vip:FindChild("VipText"):GetComponent("Text").text = string.format(UIConst.Text.VIP_DOUBLE_FORMAT, self.check_month_data.vip_level_request[index])
---        else
---            vip:SetActive(false)
---        end
---
---        item:FindChild("DayText"):GetComponent("Text").text = string.format(UIConst.Text.CHECK_DAY_FORMAT, i)
---
---        can_check_image:SetActive(false)
---        have_check_image:SetActive(false)
---        make_up:SetActive(false)
---
---        if state == CSConst.RewardState.unpick then
---            self:AddClick(item, function()
---                SpecMgrs.ui_mgr:ShowItemPreviewUI(self.check_month_data.reward_id[i])
---            end)
---        elseif state == CSConst.RewardState.pick then
---            if self.day == i then
---                can_check_image:SetActive(true)
---                local gold_effect_parent = item
---                local glod_circle_effect = UIFuncs.AddGlodCircleEffect(self, item)
---                self.cur_frame_effect_dict[glod_circle_effect] = gold_effect_parent
---            else
---                make_up:SetActive(true)
---            end
---            self:AddClick(item, function()
---                self:ClickMonthCheck(item, i)
---            end)
---        elseif state == CSConst.RewardState.picked then
---            have_check_image:SetActive(true)
---            self:AddClick(item, function()
---                SpecMgrs.ui_mgr:ShowItemPreviewUI(self.check_month_data.reward_id[i])
---            end)
---        end
-    end
---    local rect = self.check_item_list:GetComponent("RectTransform")
---    rect.anchoredPosition = Vector2.New(rect.anchoredPosition.x, 0)
-end
-
 function LoverGiftUI:InitTaskInfo()
 
     self.left_btn:SetActive(false)
@@ -244,22 +159,16 @@ function LoverGiftUI:InitTaskInfo()
 
     self.index = 1
     self:UpdateMiddle(1)
-
 end
 
 function LoverGiftUI:ClearInfo()
-    for _, item in pairs(self.cur_frame_obj_list) do
-        self:DelUIObject(item)
-    end
-    self.cur_frame_obj_list = {}
+    self:DelObjDict(self.cur_frame_obj_list)
 end
 
 function LoverGiftUI:ClearRes()
     self:ClearInfo()
     self:ClearUnit("unit")
     self.index = 1
-
 end
-
 
 return LoverGiftUI
